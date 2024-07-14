cd9e332fad9ca821ddfa08f524a148be2127976a
nineosu <nineosu@gmail.com>
2024-07-14 23:50:10 +0500

    fixes

--- js/script.js
+++ js/script.js
@@ -82,6 +82,67 @@ window.addEventListener('DOMContentLoaded', () => {
           prevEl: '.team-prev',
         },
     });
+
+    const accountSlider = new Swiper('.account-slider', {
+        direction: 'horizontal',
+        loop: false,
+        centeredSlides: true,
+        slidesPerView: 1,
+        spaceBetween: 0,
+
+        breakpoints: {
+            768: {
+                slidesPerView: 3
+            }
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.account-next',
+          prevEl: '.account-prev',
+        },
+    });
+
+    const historySlider = new Swiper('.history-swiper', {
+        enabled: true,
+        direction: 'horizontal',
+        slidesPerView: 3,
+        loop: false,
+        centeredSlides: false,
+
+        breakpoints: {
+            992: {
+                slidesPerView: 9,
+            },
+            768: {
+                loop: false,
+                slidesPerView: 6,
+                spaceBetween: 0,
+            }
+        },
+
+        navigation: {
+            nextEl: '.history-next',
+            prevEl: '.history-prev',
+          },
+    });
+
+    const feedbackSlider = new Swiper('.feedback-slider', {
+        direction: 'horizontal',
+        slidesPerView: 1,
+        spaceBetween: 30,
+
+        breakpoints: {
+            768: {
+                slidesPerView: 1.2,
+            }
+        },
+
+        navigation: {
+            nextEl: '.feedback-next',
+            prevEl: '.feedback-prev',
+          },
+    });
     
     const burger = document.querySelector('#nav-burger'),
           headerNav = document.querySelector('.header__nav');
@@ -90,6 +151,59 @@ window.addEventListener('DOMContentLoaded', () => {
         burger.classList.toggle('open');
         headerNav.classList.toggle('nav-shown');
         document.body.classList.toggle('modal-open');
+    });
+
+    // modal
+    if (document.querySelector('.modal')) {
+        const modals = document.querySelectorAll('.modal'),
+              modalBtns = document.querySelectorAll('.modal-btn');
+
+        modalBtns.forEach(btn => {
+            btn.addEventListener('click', () => {
+                modals.forEach(modal => {
+                    if (btn.getAttribute('data-modal') == modal.getAttribute('data-modal')) {
+                        modal.classList.add('show');
+                        document.documentElement.style.overflow = 'hidden';
+                        document.body.style.overflow = 'hidden';
+                    }
+                });
+            });
+        });
+
+        modals.forEach(modal => {
+            const closeBtns = modal.querySelectorAll('.modal-close');
+            
+            closeBtns.forEach(btn => {
+                btn.addEventListener('click', () => {
+                    modal.classList.remove('show');
+                    document.documentElement.style.overflow = '';
+                    document.body.style.overflow = ''; 
+                });
+            })
+        });
+        
+        modals.forEach(modal => {
+            modal.addEventListener('click', (e) => {
+                if (e.target == modal) {
+                    modal.classList.remove('show');
+                    document.documentElement.style.overflow = '';
+                    document.body.style.overflow = ''; 
+                }
+            }); 
+        });
+    }
+
+    // account login
+    const loginModal = document.querySelector('.account-login');
+    const loginSwitches = loginModal.querySelectorAll('.account-login__switch');
+    const loginModalBlocks = loginModal.querySelectorAll('.account-login__block');
+
+    loginSwitches.forEach(item => {
+        item.addEventListener('click', () => {
+            loginModalBlocks.forEach(block => {
+                block.classList.toggle('active-type');
+            });
+        });
     });
     
     // Faq
@@ -189,6 +303,24 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         });
     }
+
+    // account list
+    if (document.querySelector('.account__list')) {
+        const accountList = document.querySelector('.account__list'),
+              accountListItems = accountList.querySelectorAll('.account__list-item');
+
+        accountListItems.forEach(item => {
+            console.log()
+            item.addEventListener('click', (e) => {
+                console.log(e.target)
+                if (e.target.classList.contains('active-tab')) {
+                    accountList.classList.add('expanded');
+                } else {
+                    accountList.classList.remove('expanded');
+                }
+            });
+        });
+    }
     
     // Catalog 
     
@@ -239,6 +371,150 @@ window.addEventListener('DOMContentLoaded', () => {
                     btn.classList.remove('btn-active')
                     btnText.textContent = notInCard;
                 }
+            });
+        });
+    }
+
+    // password show
+    if (document.querySelector('.password-container')) {
+        const passwordContainer = document.querySelectorAll('.password-container');
+
+        passwordContainer.forEach(container => {
+            const toggleBtn = container.querySelector('.toggle-password'),
+                input = container.querySelector('input'),
+                ico = container.querySelector('img');
+
+            toggleBtn.addEventListener('click', () => {
+                if (input.type == 'password') {
+                    input.type = 'text';
+                    ico.src = './img/password-hide.svg'
+                } else {
+                    input.type = 'password';
+                    ico.src = './img/password-show.svg'
+                }
+            });
+        });
+    }
+
+    // categories
+
+    if (document.querySelector('.personalization__category')) {
+        const categories = document.querySelectorAll('.personalization__category');
+
+        categories.forEach(category => {
+            category.addEventListener('click', () => {
+                category.classList.toggle('active-item');
+            });
+        });
+    };
+
+    // Tabs
+
+    if (document.querySelector('.tabs__list') || document.querySelector('.tabs__items')) {
+        const tabsListItems = document.querySelectorAll('.tabs__list-item');
+        const tabsItems = document.querySelectorAll('.tabs__item');
+        
+        tabsListItems.forEach(item => {
+            item.addEventListener('click', () => {
+                tabsListItems.forEach(listItem => {
+                    listItem.classList.remove('active-tab');
+                });
+
+                item.classList.add('active-tab');
+
+                const selectedTab = item.getAttribute('data-tab');
+
+                tabsItems.forEach(tabsItem => {
+                    if (tabsItem.getAttribute('data-tab') === selectedTab) {
+                        tabsItems.forEach(item => {
+                            item.classList.remove('active-tab');
+                        });
+                        
+                        tabsItem.classList.add('active-tab');
+                    }
+                });
+            });
+        });
+    }
+
+    // account order list
+    if (document.querySelector('.account__order')) {
+        const orders = document.querySelectorAll('.account__order');
+
+        orders.forEach(order => {
+            const orderBtn = order.querySelector('.account__order-more'),
+                  orderContent = order.querySelector('.order__content');
+
+            orderBtn.addEventListener('click', () => {
+                orderBtn.classList.toggle('activeBtn');
+                orderContent.classList.toggle('show');
+            });
+        });
+    }
+
+    // pagination
+    const paginations = document.querySelectorAll('.pagination');
+
+    paginations.forEach(item => {
+        const prevBtn = item.querySelector('.pagination-prev');
+        const nextBtn = item.querySelector('.pagination-next');
+        const pageButtons = item.querySelectorAll('.pagination button:not(#prevBtn):not(#nextBtn)');
+        
+    
+        function updateButtons() {
+            const currentPage = document.querySelector('.pagination button.active');
+            const currentPageIndex = Array.from(pageButtons).indexOf(currentPage);
+    
+            prevBtn.classList.toggle('disabled', currentPageIndex === 0);
+            nextBtn.classList.toggle('disabled', currentPageIndex === pageButtons.length - 1);
+        }
+    
+        prevBtn.addEventListener('click', () => {
+            const currentPage = document.querySelector('.pagination button.active');
+            const prevPage = currentPage.previousElementSibling;
+            if (prevPage && prevPage !== prevBtn) {
+            currentPage.classList.remove('active');
+            prevPage.classList.add('active');
+            updateButtons();
+            }
+        });
+    
+        nextBtn.addEventListener('click', () => {
+            const currentPage = document.querySelector('.pagination button.active');
+            const nextPage = currentPage.nextElementSibling;
+            if (nextPage && nextPage !== nextBtn) {
+            currentPage.classList.remove('active');
+            nextPage.classList.add('active');
+            updateButtons();
+            }
+        });
+    
+        // Инициализация состояния кнопок при загрузке
+        updateButtons();
+    });
+
+    // account types
+    if (document.querySelector('.accountTypeBtn')) {
+        const btns = document.querySelectorAll('.accountTypeBtn'),
+              forms = document.querySelectorAll('.formBlock');
+
+        btns.forEach(btn => {
+            btn.addEventListener('click', () => {
+                btns.forEach(btn => {
+                    btn.classList.remove('active-type');
+                });
+                
+                btn.classList.add('active-type');
+
+                forms.forEach(form => {
+                    form.classList.remove('active-type');
+                });
+
+                forms.forEach(form => {
+                    if (btn.getAttribute('data-type') == form.getAttribute('data-type')) {
+                        form.classList.add('active-type');
+                    }
+                });
             });
         });
     }
